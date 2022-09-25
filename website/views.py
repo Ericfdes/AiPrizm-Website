3f32bb3d66929140a6f16ebc23504f9de5875892
Ericfdes <ericfergoa@gmail.com>
2022-09-25 18:53:35 +0530

    added form

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 
 from django.shortcuts import render
 from .models import Infocounter, ServiceDescription, Contact
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, Http404
+from .forms import ContactForm
 
 
 
@@ -15,13 +18,35 @@ def index(request):
     }
     return render(request,'website/index.html', context)
 
+
+
 def contact(request):
    
-    contact=Contact.objects.all()
-    context={ 
-        "contact": contact
-    }
-    return render (request,'website/contact.html',context)
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            print(form.cleaned_data)
+            body = {
+                'name': form.cleaned_data['name'],
+                'subject': form.cleaned_data['subject'],
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            # return HttpResponse("Your query has been sent.", status=200)
+            return HttpResponse("Message sent successfully", status=200)
+    
+    
+    form = ContactForm()
+    return render (request,'website/contact.html',{"form":form})
+
+
 
 def pricing(request):
     return render (request,'website/pricing.html')
