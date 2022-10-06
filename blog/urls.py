48af5c31fcf666f71dc98af9227f6dd2bf6b86aa
Ericfdes <ericfergoa@gmail.com>
2022-10-06 22:07:52 +0530

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-path('blog/',views.blog,name="blog")
+path('blog/',views.blog,name="blog"),
+path('blog_view/',views.blog_view,name="blogview")
 
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 def blog(request):
     return render(request,"blog/blog-sidebar.html")
 
+
+
+def blog_view(request):
+    return render(request,"blog/blog-single.html")
 # Create your views here.
